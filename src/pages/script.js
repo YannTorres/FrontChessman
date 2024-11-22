ff49bd014a4ce1d4f574faaf13a10c4d64db36b7
Yann <yanngames68@gmail.com>
2024-11-22 17:52:11 -0300

    novas features

--- src/pages/script.js
+++ src/pages/script.js
@@ -3,10 +3,9 @@ let index = 0;
 function showNextCard() {
     const cards = document.querySelector('.main-cards');
     const totalCards = document.querySelectorAll('.card').length;
-    index = (index + 1) % (totalCards - 1); // Ajuste para não ultrapassar o número de cards
-    const offset = -index * 50; // Mover para a esquerda (cada card ocupa 50%)
+    index = (index + 1) % (totalCards - 1); 
+    const offset = -index * 50; 
     cards.style.transform = `translateX(${offset}%)`;
 }
 
-// Muda a imagem a cada 3 segundos
 setInterval(showNextCard, 3000);
